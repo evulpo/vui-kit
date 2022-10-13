354c3ba7c754148f90b097a5085645de04d04d56
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-10-13 14:28:03 +0200

    basic bundle in dist folder using vite and rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,27 @@
-import { resolve } from 'path'
+/** @type {import('rollup').RollupOptions} */
+import { join, resolve } from 'path'
 import { defineConfig } from 'vite'
+import { RollupOptions } from "rollup";
 // import vue from '@vitejs/plugin-vue'
 import laravel from 'vite-plugin-laravel'
 
 export default defineConfig({
+	// root: join(__dirname, "src"),
 	build: {
+		outDir: join(__dirname, 'dist'),
 		lib: {
 		  entry: resolve(__dirname, 'resources/js/vui-kit.js'),
 		  name: 'VuiKit',
 		  fileName: (format) => `vui-kit.${format}.js`
 		},
+		rollupOptions: {
+			output: {
+			  	assetFileNames: (assetInfo) => {
+					if (assetInfo.name === 'style.css') return 'vui-kit.css';
+					return assetInfo.name;
+				  },
+			},
+		}
 	},
 	plugins: [
 		// laravel()
