41c25331d9ee1f69ca5885691fa3cb32179754fa
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-10-13 17:05:11 +0200

    add props to button component view

--- src/views/components/button.blade.php
+++ src/views/components/button.blade.php
@@ -1,3 +1,12 @@
+@props([
+    'sm' => false,
+    'md' => false,
+    'xl' => false,
+    'link' => false,
+    'gray' => false,
+    'darkGray' => false,
+])
+
 <div class="py-4">
     <button 
         {{-- class="inline-flex items-center px-2 lg:px-4 py-2 lg:py-3 border-2 rounded-md border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white focus:outline-none disabled:opacity-50 fox-transition text-sm lg:text-base font-medium" --}}
