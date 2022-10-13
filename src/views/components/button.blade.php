7ee01bc246b32da88167dbc731bb26a9d7d745c3
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-10-13 10:07:41 +0200

    modified hover color with iris-600

--- src/views/components/button.blade.php
+++ src/views/components/button.blade.php
@@ -1,6 +1,6 @@
 <div class="py-4">
     <button 
-        class="inline-flex items-center px-2 lg:px-4 py-2 lg:py-3 border-2 border-primary-purple rounded-md text-iris-500 hover:bg-blue-500 active:bg-blue-700 hover:text-primary-purple active:text-primary-purple focus:outline-none disabled:opacity-50 fox-transition text-sm lg:text-base font-medium"
+        class="inline-flex items-center px-2 lg:px-4 py-2 lg:py-3 border-2 border-primary-purple rounded-md text-iris-500 hover:bg-iris-700 active:bg-blue-700 hover:text-iris-600 active:text-primary-purple focus:outline-none disabled:opacity-50 fox-transition text-sm lg:text-base font-medium"
         {{-- id= --}}
         {{-- class= --}}
         {{-- aria-label --}}
