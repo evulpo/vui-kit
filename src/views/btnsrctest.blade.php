208f6e6ec2ad34a415c7fa6502a22d008a8e2665
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-10-20 14:37:58 +0200

    update btnsrctest component hover state and padding

--- src/views/btnsrctest.blade.php
+++ src/views/btnsrctest.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button class="border-2 border-black p-2"
+    <button class="border-2 border-black py-2 px-4 hover:bg-main hover:text-white"
     {{-- {{ $attributes->class(['bn bn-'.$size.' bn-'.$color])->merge(['type' => 'button']) }} --}}
     >
         button from src/views/   
