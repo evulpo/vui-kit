f52e51cbaeaa9cad8f9d5df578f771e1c73fce3c
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-10-31 12:00:49 +0100

    update button and alert

--- src/views/components/alert.blade.php
+++ src/views/components/alert.blade.php
@@ -1,17 +1,20 @@
+@props([
+    'type' => '',
+    'closable' => true
+])
+
 <div 
     class="m-1" 
     x-data="{open:true}" 
-    {{-- x-on:btuialert.window="" --}}
+    {{-- x-on:vuialert.window="" --}}
     >
 
     <div x-show.transition="open" class=" alert 
         {{-- {{ $type }} --}}
     ">
         <div class="alert-content inline-flex">
-
             <span class="alert-icon ">
                 <x-vui::icons.academic-cap class="min-h-full min-w-full h-8 w-8"/>
-
                 <i class="hidden
                     {{-- {{ $icon }} --}}
                 "> icon </i>
@@ -23,15 +26,13 @@
                     {{-- {{ $text }} --}}
                 </p>
             </div>
-
         </div>
-        {{-- @if($closable) --}}
-        <div class="alert-closebutton">
 
-            <span x-on:click="open = false" class="text-4xl">&times</span>
-
-        </div>
-        {{-- @endif --}}
+        @if($closable)
+            <div class="alert-closebutton">
+                <span x-on:click="open = false" class="text-4xl">&times</span>
+            </div>
+        @endif
     </div>
 
 </div>
--- src/views/components/button.blade.php
+++ src/views/components/button.blade.php
@@ -1,47 +1,69 @@
 @props([
+    'nav' => true,
     'size' => 'sm', 'md', 'lg',
-    'type' => 'primary', 'secondary', 'error', 'success',
-    'link' => '',
+    'type' => 'primary', 'secondary', 'error', 'success', 'filled', 'outlined',
     'sm' => false,
     'md' => false,
     'lg' => false,
+    'link' => '',
     '_blank' => false,
     'gray' => false,
     'darkGray' => false,
     'disabled' => false,
+    'disabled' => false,
 ])
 
 <div class="py-4">
-    <button 
-        {{-- class="inline-flex items-center px-2 lg:px-4 py-2 lg:py-3 border-2 rounded-md border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white focus:outline-none disabled:opacity-50 fox-transition text-sm lg:text-base font-medium" --}}
-        {{-- id= --}}
-        {{-- class= --}}
-        {{-- aria-label --}}
-        {{-- variant= --}}
-        {{-- href/link= --}}
-        {{-- color= --}}
-        {{-- size= --}}
-        {{-- startIcon= --}}
-        {{-- endIcon= --}}
-        {{-- state?= --}}
+    
+    @if( $nav )
+        <a 
+            {{ $attributes->class([
+                'px-6 py-2' => $md,
+                'px-2 py-1 text-sm' => $sm,
+                'px-10 py-3' => $lg,
+                'text-blue-500 hover:text-blue-700' => $link,
+                'bg-gray-200 hover:bg-gray-300' => $gray,
+                'bg-gray-600 hover:bg-gray-800 text-white' => $darkGray,
+                'btn-' .$size. ' btn-' .$type. ' rounded-md border-2 
+                {{-- border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white --}}
+                transition transition-all duration-300 cursor-pointer']) 
+            }} href="{{ $link }}"
+        >
+            {{ $slot }}
+        </a>         
+    @else
+        <button 
+            {{-- class="inline-flex items-center px-2 lg:px-4 py-2 lg:py-3 border-2 rounded-md border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white focus:outline-none disabled:opacity-50 fox-transition text-sm lg:text-base font-medium" --}}
+            {{-- id= --}}
+            {{-- class= --}}
+            {{-- aria-label --}}
+            {{-- variant= --}}
+            {{-- href/link= --}}
+            {{-- color= --}}
+            {{-- size= --}}
+            {{-- startIcon= --}}
+            {{-- endIcon= --}}
+            {{-- state?= --}}
 
-        {{-- {{ $attributes->class(['btn btn-'.$size  --}}
-        {{-- .'btn-'.$color --}}
-        {{-- ])->merge(['type' => 'button']) }} --}}
-        
-        {{ $attributes->class([
-            'px-6 py-2' => $md,
-            'px-2 py-1 text-sm' => $sm,
-            'px-10 py-3' => $lg,
-            'text-blue-500 hover:text-blue-700' => $link,
-            'bg-gray-200 hover:bg-gray-300' => $gray,
-            'bg-gray-600 hover:bg-gray-800 text-white' => $darkGray,
-            'btn-' .$size. ' btn-' .$type. ' rounded-md border-2 
-            {{-- border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white --}}
-            transition transition-all duration-300 cursor-pointer rounded'])->merge(['type' => 'button'])
-        }} href="{{ $link }}"
-    >
-        {{-- button src/views/components " --}}
-        {{ $slot }}
-    </button>
+            {{-- {{ $attributes->class(['btn btn-'.$size  --}}
+            {{-- .'btn-'.$color --}}
+            {{-- ])->merge(['type' => 'button']) }} --}}
+            
+            {{ $attributes->class([
+                'px-6 py-2' => $md,
+                'px-2 py-1 text-sm' => $sm,
+                'px-10 py-3' => $lg,
+                'text-blue-500 hover:text-blue-700' => $link,
+                'bg-gray-200 hover:bg-gray-300' => $gray,
+                'bg-gray-600 hover:bg-gray-800 text-white' => $darkGray,
+                'btn-' .$size. ' btn-' .$type. ' rounded-md border-2 
+                {{-- border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white --}}
+                transition transition-all duration-300 cursor-pointer']) 
+            }} href="{{ $link }}"
+        >
+            {{-- button src/views/components        --}}
+            {{ $slot }}
+        </button>
+    @endif
+
 </div>
