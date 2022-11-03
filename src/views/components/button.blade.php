16cf032ebccadd5c33eb1c3aba918e9af863989c
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-03 12:23:36 +0100

    refactor button for navigation and action role

--- src/views/components/button.blade.php
+++ src/views/components/button.blade.php
@@ -1,38 +1,23 @@
 @props([
-    'nav' => true,
+    'btn' => 'nav', 'act',
     'size' => 'sm', 'md', 'lg',
-    'type' => 'primary', 'secondary', 'error', 'success', 'filled', 'outlined',
-    'sm' => false,
-    'md' => false,
-    'lg' => false,
+    'look' => '', 'primary', 'secondary', 'error', 'success', 'filled', 'outlined',
+    // 'sm' => false,
+    // 'md' => false,
+    // 'lg' => false,
     'link' => '',
     '_blank' => false,
-    'gray' => false,
-    'darkGray' => false,
-    'disabled' => false,
+    'grey' => false,
+    'darkGrey' => false,
     'disabled' => false,
 ])
 
-<div class="py-4">
+<div class="my-4">
     
-    @if( $nav )
-        <a 
-            {{ $attributes->class([
-                'px-6 py-2' => $md,
-                'px-2 py-1 text-sm' => $sm,
-                'px-10 py-3' => $lg,
-                'text-blue-500 hover:text-blue-700' => $link,
-                'bg-gray-200 hover:bg-gray-300' => $gray,
-                'bg-gray-600 hover:bg-gray-800 text-white' => $darkGray,
-                'btn-' .$size. ' btn-' .$type. ' rounded-md border-2 
-                {{-- border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white --}}
-                transition transition-all duration-300 cursor-pointer']) 
-            }} href="{{ $link }}"
-        >
-            {{ $slot }}
-        </a>         
-    @else
+    @if( $btn == 'act' )
+        {{-- Action on the current page --}}
         <button 
+
             {{-- class="inline-flex items-center px-2 lg:px-4 py-2 lg:py-3 border-2 rounded-md border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white focus:outline-none disabled:opacity-50 fox-transition text-sm lg:text-base font-medium" --}}
             {{-- id= --}}
             {{-- class= --}}
@@ -44,26 +29,52 @@
             {{-- startIcon= --}}
             {{-- endIcon= --}}
             {{-- state?= --}}
-
             {{-- {{ $attributes->class(['btn btn-'.$size  --}}
             {{-- .'btn-'.$color --}}
             {{-- ])->merge(['type' => 'button']) }} --}}
             
             {{ $attributes->class([
-                'px-6 py-2' => $md,
-                'px-2 py-1 text-sm' => $sm,
-                'px-10 py-3' => $lg,
+                // 'px-6 py-2' => $md,
+                // 'px-2 py-1 text-sm' => $sm,
+                // 'px-10 py-3' => $lg,
                 'text-blue-500 hover:text-blue-700' => $link,
-                'bg-gray-200 hover:bg-gray-300' => $gray,
-                'bg-gray-600 hover:bg-gray-800 text-white' => $darkGray,
-                'btn-' .$size. ' btn-' .$type. ' rounded-md border-2 
-                {{-- border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white --}}
-                transition transition-all duration-300 cursor-pointer']) 
-            }} href="{{ $link }}"
+                'bg-grey-200 hover:bg-grey-300' => $grey,
+                'bg-grey-600 hover:bg-grey-800 text-white' => $darkGrey,
+                'cursor-default' => $disabled,
+                'cursor-pointer' => !$disabled,
+                'btn-' .$size. ' btn-' .$look. ' rounded-md border-2 transition transition-all duration-300'])->merge(['type' => 'button' ]) 
+            }} 
+            @if(!$disabled)
+                href="{{ $link }}"
+            @endif
         >
             {{-- button src/views/components        --}}
             {{ $slot }}
         </button>
+
+    @else
+
+        {{-- Navigates to another view --}}
+        <a 
+        {{ $attributes->class([
+            // 'px-6 py-2' => $md,
+            // 'px-2 py-1 text-sm' => $sm,
+            // 'px-10 py-3' => $lg,
+            'text-blue-500 hover:text-blue-700' => $link,
+            'border-grey-200 bg-grey-200 hover:bg-grey-300' => $grey,
+            'border-grey-500 bg-grey-600 hover:bg-grey-800 text-white' => $darkGrey,
+            'cursor-pointer' => !$disabled,
+            'cursor-default' => $disabled,
+            'btn-' .$size. ' btn-' .$look. ' rounded-md border-2 transition transition-all duration-300'])
+        }} 
+        @if(!$disabled)
+            href="{{ $link }}"
+        @endif
+            {{-- border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white --}}
+        >
+        {{ $slot }}
+        </a>
+    
     @endif
 
 </div>
