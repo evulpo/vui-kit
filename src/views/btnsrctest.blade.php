df614f6c01454557d37bdcd7ac83f78dd89c5bba
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-03 12:41:47 +0100

    update btnsrctest bg color

--- src/views/btnsrctest.blade.php
+++ src/views/btnsrctest.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button class="border-2 border-black py-2 px-4 hover:bg-main hover:text-white"
+    <button class="border-2 border-black py-2 px-4 hover:bg-grey-600 hover:text-white"
     {{-- {{ $attributes->class(['bn bn-'.$size.' bn-'.$color])->merge(['type' => 'button']) }} --}}
     >
         button from src/views/   
