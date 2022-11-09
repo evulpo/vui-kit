84ffd9c83b49fcf31ef04b73853dd86c71c89b2b
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-09 14:32:35 +0100

    add basic card component

---
+++ src/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-white relative md:w-64 md:h-[400px]  w-11/12 md:flex flex-col justify-between rounded-2xl shadow-lg hover:shadow-2xl transition-shadow m-2 mobile:w-full mobile:mx-0">
+
+    {{ $slot }}
+
+</div>
