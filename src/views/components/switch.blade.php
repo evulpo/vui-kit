7b640e23a9b63ff2ee62f6aa60fce59f4d817a89
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-09 15:08:35 +0100

    update switch with attributes merge

--- src/views/components/switch.blade.php
+++ src/views/components/switch.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div {{ $attributes->class([''])->merge(['type' => '']) }}>
     
     <div class="relative inline-block min-w-10 w-10 mr-2 align-middle select-none transition duration-200 ease-in">
         <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-2 p-2 appearance-none cursor-pointer"/>
