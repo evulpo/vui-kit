3e4f65abff6241efa25e8fd106522ec45215fa1c
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-10 13:47:27 +0100

    update modal content

--- src/views/components/modal.blade.php
+++ src/views/components/modal.blade.php
@@ -1,3 +1,7 @@
+@props([
+    'align' => true,
+])
+
 <div>
     {{-- <button></button> --}}
 
@@ -8,29 +12,27 @@
             <x-vui::button size="lg" btn="act" look="secondary">Trigger Me</x-vui::button>
         </div>
 
-        <div x-show.transition="open" class="fixed z-50 top-0 bottom-0 left-0 right-0 flex bg-white bg-opacity-10 py-32 {{ $align ?? "justify-center" }}">
+        <div x-show.transition="open" class="fixed z-50 top-0 bottom-0 left-0 right-0 flex bg-white bg-opacity-10 py-32 rounded-lg {{ $align ?? "justify-center" }}">
 
             <div x-show.transition="open"
                 @click.away="open = false"
                 class="grid grid-cols-1 w-screen h-screen/60 min-h-screen/60 max-w-3/5 min-w-96
                 {{-- {{ 'bt-w-'.$width }} --}}
-                 bg-white border-gray-200 shadow-lg overflow-y-auto mx-auto">
+                 bg-white border-gray-200 shadow-lg overflow-y-auto mx-auto rounded-lg">
     
-                <div class="w-full">
-                    <div class="flex justify-between px-4 py-2 bg-gray-100">
-                        <div class="py-2 text-xl text-black">
+                <div class="w-full rounded-lg">
+                    <div class="flex justify-end top-0 right-0 w-full">
+                        {{-- Close Button --}}
+                        <button @click="open = false" class="w-8- h-8 px-2 py-2 text-3xl text-gray-800 transition-all hover:text-4xl hover:text-red-500">&times;</button>
+                    </div>
+                    <div class="flex flex-col justify-center w-full px-8 py-2 bg-gray-100">
+                        <div class="py-2 px-4 text-xl text-black">
                             {{-- {{ $title }} --}}
                         </div>
-                        <div class="">
-                            {{-- Close Button --}}
-                            <button @click="open = false" class="w-8- h-8 px-1 py-1 text-3xl text-gray-800 transition-all hover:text-4xl hover:text-red-500">&times;</button>
+                        <div class="text-black">
+                            {{ $slot }}
                         </div>
-
                     </div>
-                    <div class="text-black">
-                        {{ $slot }}
-                    </div>
-
                 </div>
 
             </div>
