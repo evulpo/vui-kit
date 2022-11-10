831cee391648a33611b1f2a7604234f7cf675fc1
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-10 12:41:48 +0100

    replace trigger text with button component

--- src/views/components/modal.blade.php
+++ src/views/components/modal.blade.php
@@ -2,15 +2,16 @@
     {{-- <button></button> --}}
 
     <div x-data="{open: false}" x-cloak>
-        {{-- trigger --}}
+        {{-- Trigger Button --}}
         <div @click="open = true" class="w-full">
             {{-- {{ $trigger }} --}}
-            Trigger Button
+            <x-vui::button size="lg" btn="act" look="secondary">Trigger Me</x-vui::button>
         </div>
 
         <div x-show.transition="open" class="fixed z-50 top-0 bottom-0 left-0 right-0 flex bg-white bg-opacity-10 py-32 {{ $align ?? "justify-center" }}">
 
             <div x-show.transition="open"
+                @click.away="open = false"
                 class="grid grid-cols-1 w-screen h-screen/60 min-h-screen/60 max-w-3/5 min-w-96
                 {{-- {{ 'bt-w-'.$width }} --}}
                  bg-white border-gray-200 shadow-lg overflow-y-auto mx-auto">
@@ -21,7 +22,7 @@
                             {{-- {{ $title }} --}}
                         </div>
                         <div class="">
-                            {{-- close button --}}
+                            {{-- Close Button --}}
                             <button @click="open = false" class="w-8- h-8 px-1 py-1 text-3xl text-gray-800 transition-all hover:text-4xl hover:text-red-500">&times;</button>
                         </div>
 
