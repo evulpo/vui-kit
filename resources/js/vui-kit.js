50a8523f289c31c802bd72cd238bf7561086e179
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-17 11:18:12 +0100

    change assets files

--- resources/js/vui-kit.js
+++ resources/js/vui-kit.js
@@ -1,9 +1,9 @@
 // scripts
 // vite ?
-
 import '../css/vui-kit.css'; 
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
  
-window.Alpine = Alpine
+window.Alpine = Alpine;
  
-Alpine.start()
+Alpine.start();
+
