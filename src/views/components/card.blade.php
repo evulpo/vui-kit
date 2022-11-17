9dafdab43a5c46c5ffa253721e430b1019662e74
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-17 09:51:33 +0100

    update card view

--- src/views/components/card.blade.php
+++ src/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white relative md:w-64 md:h-[400px]  w-11/12 md:flex flex-col justify-between rounded-2xl shadow-lg hover:shadow-2xl transition-shadow m-2 mobile:w-full mobile:mx-0">
+<div class="bg-white relative md:min-h-[400px] w-11/12 md:flex flex-col justify-between rounded-xl shadow-lg hover:shadow-2xl transition-shadow m-2 mobile:w-full mobile:mx-0">
 
     {{ $slot }}
 
