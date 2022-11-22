62c644e69e928c97f5d8d4a4cc0ff53552059e29
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-22 15:53:51 +0100

    update tooltip size

--- src/views/components/tooltip.blade.php
+++ src/views/components/tooltip.blade.php
@@ -1,4 +1,4 @@
-<div class="cursor-pointer flex justify-end" @click="showModal= true ">
-    <x-vui::icons.help class="w-8 h-8 lg:mt-1 mx-2" ></x-vui::icon.help>
+<div class="cursor-pointer flex justify-end w-8 h-8" @click="showModal= true ">
+    <x-vui::icons.help class=" lg:mt-1 mx-2" ></x-vui::icon.help>
     {{-- <img alt='help' class="w-8 h-8 lg:mt-1 mx-2" src="{{ asset('icons/help.svg') }}" /> --}}
 </div>
