32d8e0234853a3bdd6f859b6c337d2b8dd04473a
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-22 16:38:06 +0100

    update tooltip with mousehover

--- src/views/components/tooltip.blade.php
+++ src/views/components/tooltip.blade.php
@@ -1,4 +1,14 @@
-<div class="cursor-pointer flex justify-end w-8 h-8" @click="showModal= true ">
-    <x-vui::icons.help class=" lg:mt-1 mx-2" ></x-vui::icon.help>
-    {{-- <img alt='help' class="w-8 h-8 lg:mt-1 mx-2" src="{{ asset('icons/help.svg') }}" /> --}}
+<div x-data="{ hover: false}" >
+    <div 
+        x-on:mouseover="hover = true" 
+        x-on:mouseout="hover = false"
+        class="cursor-pointer flex justify-end w-8 h-8" @click="showModal= true ">
+        <x-vui::icons.help class=" lg:mt-1 mx-2" ></x-vui::icon.help>
+        {{-- <img alt='help' class="w-8 h-8 lg:mt-1 mx-2" src="{{ asset('icons/help.svg') }}" /> --}}
+    </div>
+
+    <div x-show="hover" class="absolute right-8 top-8 w-60 transition-all border-2 border-primary-purple-light duration-150 ease-in-out filter drop-shadow bg-white overflow-auto rounded-xl py-4 px-4 z-20 font-light" style="display: none;"> 
+        {{ $slot }}
+    </div>
+    
 </div>
