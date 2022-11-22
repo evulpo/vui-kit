7f27751b3f699211086304765c973def39f77c9c
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-22 13:37:57 +0100

    clean button component

--- src/views/components/button.blade.php
+++ src/views/components/button.blade.php
@@ -3,7 +3,6 @@
     'size' => 'lg', 'md', 'sm',
     'variant' => 'filled', 'outlined', 'text',
     'color' => 'iris', 'jade', 'red', 'grey', 
-    'look' => '', 'primary', 'secondary', 'error', 'success', 'filled', 'outlined',
     'link' => '',
     'name' => '',
     'disabled' => false,
@@ -12,11 +11,6 @@
     'endIcon' => false,
     'icon' => '',
     'loading' => false,
-    // 'sm' => false,
-    // 'md' => false,
-    // 'lg' => false,
-    'grey' => false,
-    'darkGrey' => false,
 ])
 
 <div class="my-4">
@@ -41,21 +35,18 @@
             {{-- ])->merge(['type' => 'button']) }} --}}
             
             {{ $attributes->class([
-                'text-blue-500 hover:text-blue-700 underline' => $link,
-                'text-'.$color.'-500 dark:text-white' => $variant == 'text',
-                'border-2 border-'.$color.'-500 bg-'.$color.'-500 text-white hover:text-'.$color.'-500 hover:bg-white ' => $variant == 'filled',
+                $variant == 'text' ? 'text-'.$color.'-500 dark:text-white' : '',
+                $variant == 'filled' ? 'border-2 border-'.$color.'-500 bg-'.$color.'-500 text-white hover:text-'.$color.'-500 hover:bg-white ' : '',
                 $variant == 'outlined' ? 'border-2 border-'.$color.' text-'.$color.'-500 hover:text-white bg-white hover:bg-'.$color.'-500' : '',
-                // 'border-grey-200 bg-grey-200 hover:bg-grey-300' => $grey,
-                // 'border-grey-500 bg-grey-600 hover:bg-grey-800 text-white' => $darkGrey,
+                'disabled cursor-default opacity-80' => $disabled,
                 'cursor-pointer' => !$disabled,
-                'disabled cursor-default bg-grey-200 hover:bg-grey-200 text-grey-300 hover:text-grey-300 opacity-80 border-grey-300' => $disabled,
+                'text-blue-500 hover:text-blue-700 underline' => $link,
                 'btn-' .$size.' btn-'.$variant.' btn-'.$color.' rounded-md transition transition-all duration-300'])
             }} 
             {{-- @if(!$disabled )
                 href="{{ $link }}"
             @endif --}}
         >
-            {{-- button src/views/components --}}
             {{ $slot }}
         </button>
 
@@ -63,23 +54,20 @@
 
         {{-- Navigates to another view --}}
         <a 
-        {{ $attributes->class([
-            'text-blue-500 hover:text-blue-700 underline' => $link,
-            'text-'.$color.'-500 dark:text-white' => $variant == 'text',
-            'border-2 border-'.$color.'-500 bg-'.$color.'-500 text-white hover:text-'.$color.'-500 hover:bg-white ' => $variant == 'filled',
-            $variant == 'outlined' ? 'border-2 border-'.$color.' text-'.$color.'-500 hover:text-white bg-white hover:bg-'.$color.'-500' : '',
-            // 'border-grey-200 bg-grey-200 hover:bg-grey-300' => $grey,
-            // 'border-grey-500 bg-grey-600 hover:bg-grey-800 text-white' => $darkGrey,
-            'cursor-pointer' => !$disabled,
-            'disabled cursor-default bg-grey-200 hover:bg-grey-200 text-grey-300 hover:text-grey-300 opacity-80 border-grey-300' => $disabled,
-            'btn-' .$size.' btn-'.$variant.' btn-'.$color.' rounded-md transition transition-all duration-300'])
-        }} 
-        @if(!$disabled)
-            href="{{ $link }}"
-        @endif
-            {{-- border-iris-500 text-iris-500 hover:bg-iris-500 hover:text-white active:bg-iris-600 active:text-white --}}
+            {{ $attributes->class([
+                $variant == 'text' ? 'text-'.$color.'-500 dark:text-white' : '',
+                $variant == 'filled' ? 'border-2 border-'.$color.'-500 bg-'.$color.'-500 text-white hover:text-'.$color.'-500 hover:bg-white ' : '',
+                $variant == 'outlined' ? 'border-2 border-'.$color.' text-'.$color.'-500 hover:text-white bg-white hover:bg-'.$color.'-500' : '',
+                'disabled cursor-default opacity-80' => $disabled,
+                'cursor-pointer' => !$disabled,
+                'text-blue-500 hover:text-blue-700 underline' => $link,
+                'btn-' .$size.' btn-'.$variant.' btn-'.$color.' rounded-md transition transition-all duration-300'])
+            }}  
+            @if(!$disabled)
+                href="{{ $link }}"
+            @endif
         >
-        {{ $slot }}
+            {{ $slot }}
         </a>
     
     @endif
