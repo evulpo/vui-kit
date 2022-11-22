10a92b59e8e8814da812dda1ff882532aa56f8f0
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-22 13:42:52 +0100

    correct dropdown view

--- src/views/components/dropdown.blade.php
+++ src/views/components/dropdown.blade.php
@@ -1,45 +1,26 @@
 @props([
-    'size' => 'sm', 'md', 'lg',
-    'type' => 'text', 'email', 'tel', 'password', 'date', 'time', 'url', 'color', 'range',
-    'placeholder' => '',
-    'label' => '',
-    'name' => '',
-    'icon' => false,
-    'disabled' => false
-])
+    'align' => 'left',
+    'trigger' => '',
+    'triggerClass' => ''
+    ])
 
-<div 
-    {{-- class="min-w-1/6 my-4 pr-4 flex flex-col justify-end"  --}}
-    {{ $attributes->merge([ 'class' => 'my-4 pr-4 flex flex-col justify-end' ]) }}
-    >
-    <x-vui::label >{{ $label }}</x-vui::label>
-    <input 
-        class="h-14 p-2 rounded-lg input input-{{ $type }}"
-        {{ $attributes->merge(['type' => $type]) }}
-        {{-- type='' --}}
-        placeholder="{{ $placeholder }} input {{ $type }}"
-    >
-</div>
-
-
-<div class="hidden">
-    <div class="input-wrap 
-    {{-- input-wrap-{{ $type }} --}}
-    ">       
-        <div class="w-full">
-            <input {{ $attributes->class(['rounded-lg input input-'
-            //. $size 
-            ])->merge(['type' => 'text']) }} placeholder="test"> 
-        </div>    
-        {{-- @if($icon != false) --}}
-        <div class="input-icon 
-        {{-- input-{{ $size }} --}}
-        ">
-            <i class="
-            {{-- {{ $icon }} --}}
-            ">
-            </i>
+<div >
+    <div x-data="{open:false}" class="relative max-w-max w-full" x-cloak>
+        <div x-on:click="open = !open" class=" {{ $triggerClass }} p-2 cursor-pointer max-w-max dark:text-iris-100">
+            {{ $trigger ?? '' }}
         </div>
-        {{-- @endif --}}
+
+        <div x-show="open" x-on:click.away="open = false"
+            x-transition:enter="transition ease-out duration-100 origin-top"
+            x-transition:enter-start="opacity-0 scale-90"
+            x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="transition ease-in duration-100 origin-top"
+            x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-90"
+
+            class="absolute {{ $align == 'left' ? 'left-0' : 'right-0' }} bg-white rounded overflow-hidden top-0 shadow-lg w-full p-4 mt-8 min-w-max"
+            style="min-width: 200px;z-index:99;">
+            {{ $slot }}
+        </div>
     </div>
 </div>
